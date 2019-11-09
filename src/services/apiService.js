dd4437116f2f2953179dd9c3ed1855d4f32a0efa
Shinies <github@morkeltry.33mail.com>
2019-11-09 20:20:21 +0000

    added fallbacks for dev environment in constants.js; Added setupProxy.js for use in c-r-a dev environment

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -4,8 +4,15 @@ import { API_URL } from '../constants'
 export const fetchUserData = async () => {
   let data = null
   try {
+    console.log(`Try: ${API_URL}/api`);
     ({ data } = await axios.get(`${API_URL}/api`, { withCredentials: true }))
+    console.log(`Tried: ${API_URL}/api`);
   } catch (e) {
+    console.log(Object.keys(e));
+    console.log(e.toJSON());
+    console.log(e);
+    console.log(e);
+    console.log('GET Error:',e);
     if (e.response && e.response.status) {
       if (e.response.status === 401 || e.response.status === 403) {
         return null
@@ -20,7 +27,7 @@ export const fetchUserData = async () => {
     return null
   }
   if (data.errors) {
-    throw new Error(data.errors.map(e => e.message).join(', '))
+    throw new Error(data.errors.map(e => e.toString()).join(', '))
   }
   return data
 }
