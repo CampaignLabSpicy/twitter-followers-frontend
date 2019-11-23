2f048093effbb14bab24f92c8481da21751f9c44
Shinies <github@morkeltry.33mail.com>
2019-11-23 22:01:02 +0000

    retrieve location with function fetchPostcodeData(pc)

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,22 +1,35 @@
 import axios from "axios";
 import { ACTIVIST_THRESHOLD, API_URL } from "../constants";
 
+const handleTwitterErrors = e => {
+  if (e.response && e.response.status) {
+    if (e.response.status === 401 || e.response.status === 403) {
+      return null;
+    }
+    if (e.response.status === 429) {
+      throw new Error(
+        "You are being rate limited. Please try again in 15 minutes."
+      );
+    }
+  }
+  throw new Error(e);
+}
+
+export const fetchPostcodeData = async (pc) => {
+  try {
+    const { location } = await axios.get(`${API_URL}/api/?pc=${pc}`);
+    return location || null;
+  } catch (e) {
+    throw new Error(e);
+  }
+};
+
 export const fetchUserData = async () => {
   let data = null;
   try {
     ({ data } = await axios.get(`${API_URL}/api`, { withCredentials: true }));
   } catch (e) {
-    if (e.response && e.response.status) {
-      if (e.response.status === 401 || e.response.status === 403) {
-        return null;
-      }
-      if (e.response.status === 429) {
-        throw new Error(
-          "You are being rate limited. Please try again in 15 minutes."
-        );
-      }
-    }
-    throw new Error(e);
+    handleTwitterErrors(e);
   }
   if (!data) {
     return null;
@@ -27,16 +40,4 @@ export const fetchUserData = async () => {
   const percentage = Math.round((data.matched / data.total) * 100);
   const userType = percentage > ACTIVIST_THRESHOLD ? "Mobiliser" : "Converter";
   return { percentage, userType, ...data };
-};
-
-export const fetchPostcodeData = async postcode => {
-  try {
-    const response = await axios.get(`${API_URL}/api`, {
-      withCredentials: true
-    });
-
-    return response.data;
-  } catch (e) {
-    throw new Error(e);
-  }
 };
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -7,14 +7,3 @@ const proxyTarget  = {
 module.exports = function(app) {
   app.use('/api', proxy(proxyTarget));
 };
-
-// const proxy = require('http-proxy-middleware');
-// module.exports = function(app) {
-//   app.use(
-//     '/api',
-//     proxy({
-//       target: 'http://127.0.0.1:8080',
-//       changeOrigin: true,
-//     })
-//   );
-// };
